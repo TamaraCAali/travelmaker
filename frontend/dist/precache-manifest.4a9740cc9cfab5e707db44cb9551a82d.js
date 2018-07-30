89b32d213777370894a21dd0aa0a1e05acbed44e
yedidic <yedidic@gmail.com>
2018-07-30 15:13:00 +0300

    deploying

---
+++ frontend/dist/precache-manifest.4a9740cc9cfab5e707db44cb9551a82d.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "ac039aee4ec69579a64cdd63ed4db625",
+    "url": "/fonts/MavenPro-Regular.ac039aee.ttf"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "ff13575c85d0b4759aed",
+    "url": "/js/chunk-vendors.ff13575c.js"
+  },
+  {
+    "revision": "4c6167e176cf482f1038",
+    "url": "/js/app.4c6167e1.js"
+  },
+  {
+    "revision": "ef6ebc29c559553f6d2d5b950d241f70",
+    "url": "/index.html"
+  },
+  {
+    "revision": "97ba7aa0acda09d2ae217aa437043af3",
+    "url": "/img/login_Backg.97ba7aa0.jpg"
+  },
+  {
+    "revision": "6f0a76321d30f3c8120915e57f7bd77e",
+    "url": "/fonts/element-icons.6f0a7632.ttf"
+  },
+  {
+    "revision": "2fad952a20fbbcfd1bf2ebb210dccf7a",
+    "url": "/fonts/element-icons.2fad952a.woff"
+  },
+  {
+    "revision": "324944d6dae53ea48a648b1d44ec78c3",
+    "url": "/fonts/Pacifico-Regular.324944d6.ttf"
+  },
+  {
+    "revision": "ff13575c85d0b4759aed",
+    "url": "/css/chunk-vendors.9afd90ca.css"
+  },
+  {
+    "revision": "4c6167e176cf482f1038",
+    "url": "/css/app.156ab4d4.css"
+  }
+];
